3ce12dde350454062dacc107997c2e1f95a2a517
E020320104 <mhmmdaldyfirdaus024@gmail.com>
2023-02-09 20:03:00 +0800

    uas

---
+++ FDAE020320104/app/src/main/java/id/ac/poliban/E020320104/project/Activity/loginActivity.java
@@ -0,0 +1,6 @@
+package id.ac.poliban.E020320104.project.Activity;
+
+import id.ac.poliban.E020320104.project.R;
+
+public class loginActivity {
+}
---
+++ FDAE020320104/app/src/main/java/id/ac/poliban/E020320104/project/Interface/ChangeNumberItemsListener.java
@@ -0,0 +1,5 @@
+package id.ac.poliban.E020320104.project.Interface;
+
+public interface ChangeNumberItemsListener {
+    void changed();
+}
